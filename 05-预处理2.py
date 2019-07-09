8b1a965e00602f66c5ec8d969a2ac0d3c3fe5e43
Ding <38353554+nuoyi-618@users.noreply.github.com>
2019-07-09 21:17:55 +0800

    Update 05-预处理2.py

--- 05-预处理2.py
+++ 05-预处理2.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 24 15:22:24 2018
+Created on Tue May 14 23:08:45 2019
 
 @author: 丁凡彧
 """
 
 import pandas as pd
-import math
-df = pd.read_csv('../data/YCL-user-poi-246w.csv')
-
+df = pd.read_csv('../../data/YCL/YCL-user-poi-246w.csv')
+df=df.drop_duplicates(['id','poi'])
 pieces = dict(list(df.groupby('id')))
-
 name=[]
-
+#去除最后一个景点
+drop=[]
+#筛选景点数量在3-10之间的用户
 for key in pieces:
-    d=pieces[key].reset_index(drop=True)
-    d=d.drop_duplicates(['poi'])
-    if d.shape[0] >=3:
+    d=pieces[key]
+    d=d.sort_values(by=['date','time'])
+    if d.shape[0] >= 3 and d.shape[0]<=10:
         name.append(key)
-'''
-    if d.drop_duplicates(['date']).shape[0] > 2:
-        d=d[(d["time"]> 7 ) & (d["time"] < 20) ]
-        d=d.drop_duplicates(['date','poi'])
-        if d.shape[0] > 2:        
-            npoi=max(d['poi'].value_counts())
-            nid=d.drop_duplicates(['date']).shape[0]
-            if npoi/nid >= (4/7):
-                name.append(key)
-'''
+        drop.append(d.tail(n=1).index.tolist()[0])
 df=df[df['id'].isin(name)]
+df=df[~df.index.isin(drop)]
+df.to_csv('../../data/2/try(3-10)/use-poi.csv',index=False)
